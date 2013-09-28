38aa1a63d6a9d3d5294bcdbfd488a97bd3544a81
Yosiya Hinosawa <stibium121@gmail.com>
2013-09-29 01:25:52 +0900

    Add tests

---
+++ src/test/groovy/org/kt3k/gradle/plugin/coveralls/domain/ServiceInfoTest.groovy
@@ -0,0 +1,17 @@
+package org.kt3k.gradle.plugin.coveralls.domain
+
+import org.junit.Test
+import static org.junit.Assert.*
+
+class ServiceInfoTest {
+
+	@Test
+	void testConstructor() {
+		ServiceInfo serviceInfo = new ServiceInfo('x-ci', '1729')
+
+		assertNotNull serviceInfo
+		assertEquals 'x-ci', serviceInfo.serviceName
+		assertEquals '1729', serviceInfo.serviceJobId
+	}
+
+}
---
+++ src/test/groovy/org/kt3k/gradle/plugin/coveralls/domain/SourceReportTest.groovy
@@ -0,0 +1,19 @@
+package org.kt3k.gradle.plugin.coveralls.domain
+
+import org.junit.Test
+import static org.junit.Assert.*
+
+class SourceReportTest {
+	
+	@Test
+	void testConstructor() {
+		SourceReport sourceReport = new SourceReport('test.java', 'class A {\n  String a = "a";\n}', [1, 0, null])
+		
+		assertNotNull sourceReport
+
+		assertEquals 'test.java', sourceReport.name
+		assertEquals 'class A {\n  String a = "a";\n}', sourceReport.source
+		assertEquals([1, 0, null], sourceReport.coverage)
+	}
+
+}
