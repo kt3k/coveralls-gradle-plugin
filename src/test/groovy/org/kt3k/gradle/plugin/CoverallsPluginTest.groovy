99f9413f27fc8904c2cbdab526939882958a2440
Yosiya Hinosawa <stibium121@gmail.com>
2013-10-02 17:51:34 +0900

    Add plugin test

--- src/test/groovy/org/kt3k/gradle/plugin/CoverallsPluginTest.groovy
+++ src/test/groovy/org/kt3k/gradle/plugin/CoverallsPluginTest.groovy
@@ -3,11 +3,26 @@ package org.kt3k.gradle.plugin
 import org.junit.Test
 import static org.junit.Assert.*
 
+import static org.mockito.Mockito.*
+
+import org.gradle.api.Project
+import org.gradle.api.Plugin
+import org.gradle.api.Task
+
 class CoverallsPluginTest {
 
 	@Test
 	void testApply() {
-		
+		Task task = mock(Task.class)
+
+		Project project = mock(Project.class)
+		when(project.task('coveralls')).thenReturn(task)
+
+		Plugin plugin = new CoverallsPlugin()
+		plugin.apply(project)
+
+		verify(project).task('coveralls')
+		verify(task).leftShift(any(Closure))
 	}
 
 }
