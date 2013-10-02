60a6ffb3348ede366c6f6f79cea974b2810954fb
Yosiya Hinosawa <stibium121@gmail.com>
2013-10-02 21:46:47 +0900

    Add tests of SourceReportFactory

--- src/test/groovy/org/kt3k/gradle/plugin/coveralls/domain/SourceReportFactoryTest.groovy
+++ src/test/groovy/org/kt3k/gradle/plugin/coveralls/domain/SourceReportFactoryTest.groovy
@@ -1,10 +1,24 @@
 package org.kt3k.gradle.plugin.coveralls.domain
 
 import org.junit.Test
+import static org.junit.Assert.*
 
 class SourceReportFactoryTest {
-	
+
 	@Test
-	void testCreate() {
+	void testCreateFromCoberturaXML() {
+
+		List<SourceReport> reports = SourceReportFactory.createFromCoberturaXML new File('src/test/fixture/coverage.xml')
+
+		assertNotNull reports
+		assertEquals 7, reports.size()
+
+		assertEquals 'org/kt3k/gradle/plugin/CoverallsPlugin.groovy', reports[0].name
+		assertEquals 'org/kt3k/gradle/plugin/coveralls/Application.groovy', reports[1].name
+		assertEquals 'org/kt3k/gradle/plugin/coveralls/domain/Report.groovy', reports[2].name
+		assertEquals 'org/kt3k/gradle/plugin/coveralls/domain/ServiceInfo.groovy', reports[3].name
+		assertEquals 'org/kt3k/gradle/plugin/coveralls/domain/ServiceInfoFactory.groovy', reports[4].name
+		assertEquals 'org/kt3k/gradle/plugin/coveralls/domain/SourceReport.groovy', reports[5].name
+		assertEquals 'org/kt3k/gradle/plugin/coveralls/domain/SourceReportFactory.groovy', reports[6].name
 	}
 }
