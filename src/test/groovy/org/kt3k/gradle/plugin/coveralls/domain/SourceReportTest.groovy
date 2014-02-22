7de5a0ad9f4f02f1fb207189dc6a64cd1d6acbdb
Yoshiya Hinosawa <stibium121@gmail.com>
2014-02-22 15:52:40 +0900

    Delete trailing tab

--- src/test/groovy/org/kt3k/gradle/plugin/coveralls/domain/SourceReportTest.groovy
+++ src/test/groovy/org/kt3k/gradle/plugin/coveralls/domain/SourceReportTest.groovy
@@ -4,11 +4,11 @@ import org.junit.Test
 import static org.junit.Assert.*
 
 class SourceReportTest {
-	
+
 	@Test
 	void testConstructor() {
 		SourceReport sourceReport = new SourceReport('test.java', 'class A {\n  String a = "a";\n}', [1, 0, null])
-		
+
 		assertNotNull sourceReport
 
 		assertEquals 'test.java', sourceReport.name
