2bb9508e7e85a04ceab028f2c573a429a9397a59
Yoshiya Hinosawa <stibium121@gmail.com>
2014-06-21 15:18:56 +0100

    Add a note

--- src/main/groovy/org/kt3k/gradle/plugin/coveralls/domain/CoberturaSourceReportFactory.groovy
+++ src/main/groovy/org/kt3k/gradle/plugin/coveralls/domain/CoberturaSourceReportFactory.groovy
@@ -16,8 +16,10 @@ class CoberturaSourceReportFactory implements SourceReportFactory {
 		// skip external DTD validation
 		// see http://xerces.apache.org/xerces2-j/features.html#nonvalidating.load-external-dtd
 		parser.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false)
+
 		// allow DOCTYPE declaration
 		// see http://xerces.apache.org/xerces2-j/features.html#disallow-doctype-decl
+		// this is the same as the default, but in some situation it seems to turn to be true ( see https://github.com/kt3k/coveralls-gradle-plugin/pull/16 )
 		parser.setFeature("http://apache.org/xml/features/disallow-doctype-decl", false)
 
 		// parse
