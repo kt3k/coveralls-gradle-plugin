3bd4f91934381d4a11da86d44f34798ef623aa9c
Yoshiya Hinosawa <stibium121@gmail.com>
2014-08-03 16:16:54 +0100

    Add the tests of equals and hashCode of ServiceInfo

--- src/test/groovy/org/kt3k/gradle/plugin/coveralls/domain/ServiceInfoTest.groovy
+++ src/test/groovy/org/kt3k/gradle/plugin/coveralls/domain/ServiceInfoTest.groovy
@@ -15,4 +15,25 @@ class ServiceInfoTest {
 		assertEquals 'ABCDEF', serviceInfo.repoToken
 	}
 
+
+	@Test
+	void testEquals() {
+		assertEquals new ServiceInfo("a", "b", "c", "d", "e", "f"), new ServiceInfo("a", "b", "c", "d", "e", "f")
+		assertNotEquals new ServiceInfo("a", "b", "c", "d", "e", "f"), new ServiceInfo("_", "b", "c", "d", "e", "f")
+		assertNotEquals new ServiceInfo("a", "b", "c", "d", "e", "f"), new ServiceInfo("a", "_", "c", "d", "e", "f")
+		assertNotEquals new ServiceInfo("a", "b", "c", "d", "e", "f"), new ServiceInfo("a", "b", "_", "d", "e", "f")
+		assertNotEquals new ServiceInfo("a", "b", "c", "d", "e", "f"), new ServiceInfo("a", "b", "c", "_", "e", "f")
+		assertNotEquals new ServiceInfo("a", "b", "c", "d", "e", "f"), new ServiceInfo("a", "b", "c", "d", "_", "f")
+		assertNotEquals new ServiceInfo("a", "b", "c", "d", "e", "f"), new ServiceInfo("a", "b", "c", "d", "e", "_")
+	}
+
+
+	@Test
+	void testHashCode() {
+		ServiceInfo serviceInfo = new ServiceInfo("a", "b", "c", "d", "e", "f")
+
+		assertNotNull serviceInfo.hashCode()
+		assertTrue serviceInfo.hashCode().toString().isInteger()
+	}
+
 }
